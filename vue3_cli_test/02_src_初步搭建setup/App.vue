9124651920db639a68f6c162a04c1dc59069ea94
masibo <1173116613@qq.com>
2022-05-30 17:39:56 +0800

    vue3

---
+++ vue3_cli_test/02_src_初步搭建setup/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- Vue3组件中的模板结构可以没有跟标签 -->
+  <!-- 在vue2中的配置可以读取到vue3中的数据和方法,但是在vue3中不能读取到vue2中的数据和方法,vue2和vue3同时定义一个数据时,以vue3为准 -->
+<h3>{{name}}</h3>
+<h3>{{age}}</h3>
+<h3>{{sex}}</h3>
+<h3>{{a}}</h3>
+<button @click="sayHello">vue3你好</button>
+<br>
+<button @click="test1">测试一下在Vue3的setup配置中去读取Vue2中的数据、方法</button>
+<br>
+<button @click="sayNihao">vue2你好 </button>
+<br>
+<button @click="test2">测试一下在Vue2的配置中去读取Vue3中的数据、方法</button>
+
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  // vue2中的配置
+  data(){
+    return {
+      sex:'男',
+      a:200
+    }
+  },
+  methods:{
+    sayNihao(){
+      console.log(this.sex)
+      
+
+    },
+    test2 (){
+      console.log(this.age)
+      console.log(this.sayHello)
+      console.log(this.name)
+      // 
+    }
+  },
+  setup() {
+    // 相当于data中的数据
+    let name = "马嗣博";
+    let age = 18;
+    let a = 300;
+    // 方法,相当于methods
+    function sayHello() {
+      alert(`我是${name},今年${age}岁`);
+
+    }
+    function test1 (){
+      console.log(this.sex)
+      console.log(this.sayNihao)
+    }
+    return {
+      name,
+      age,
+      a,
+      sayHello,
+      test1
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3_cli_test/03_src_ref函数/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- Vue3组件中的模板结构可以没有跟标签 -->
+  <!-- 在vue2中的配置可以读取到vue3中的数据和方法,但是在vue3中不能读取到vue2中的数据和方法,vue2和vue3同时定义一个数据时,以vue3为准 -->
+<h3>{{name}}</h3>
+<h3>{{age}}</h3>
+<h3>{{sex}}</h3>
+<h3>工作类型:{{job.type}}</h3>
+<h3>工作薪资:{{job.money}}</h3>
+<button @click="changeXiaowang">变成小王</button>
+
+</template>
+
+<script>
+// 引入ref函数
+import {ref} from 'vue'
+export default {
+  name: "App",
+
+  setup() {
+    // 相当于data中的数据
+    // 通过ref实现数据响应式,ref底层还是get set 通过数据劫持实现数据响应式
+    let name = ref("马嗣博");
+    let age = ref(18);
+    let sex = ref('男');
+    let job = ref({
+      type:'前端工程师',
+      money:'30k'
+    })
+    // 方法,相当于methods
+    function changeXiaowang() {
+      name.value = '小王'
+      age.value = 18
+      sex.value = '女'
+      console.log(name)
+      console.log(job)
+    }
+
+    return {
+      // 数据
+      name,
+      age,
+      sex,
+      job,
+      // 方法
+      changeXiaowang,
+    }
+  },
+};
+</script>
+
+
